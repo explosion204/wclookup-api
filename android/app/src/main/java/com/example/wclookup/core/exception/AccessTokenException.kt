52b81feaae1e5c1abadba796f0147047524f3fe3
ChoCoBo1712 <BigBen1712@mail.ru>
2021-12-04 15:54:28 +0300

    add exception classes

---
+++ android/app/src/main/java/com/example/wclookup/core/exception/AccessTokenException.kt
@@ -0,0 +1,3 @@
+package com.example.wclookup.core.exception
+
+class AccessTokenException : Exception()
---
+++ android/app/src/main/java/com/example/wclookup/core/exception/RefreshTokenException.kt
@@ -0,0 +1,5 @@
+package com.example.wclookup.core.exception
+
+import java.lang.Exception
+
+class RefreshTokenException : Exception()
