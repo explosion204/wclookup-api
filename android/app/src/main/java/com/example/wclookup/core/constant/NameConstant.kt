aa4471f092f0f4ad86b7990913c9625ab95f2815
ChoCoBo1712 <BigBen1712@mail.ru>
2021-12-04 15:52:03 +0300

    add constants class

---
+++ android/app/src/main/java/com/example/wclookup/core/constant/NameConstant.kt
@@ -0,0 +1,28 @@
+package com.example.wclookup.core.constant
+
+object NameConstant {
+    const val ID = "id"
+    const val USER_ID = "userId"
+    const val TOILET_ID = "toiletId"
+    const val RATING = "rating"
+    const val TEXT = "text"
+    const val ADDRESS = "address"
+    const val SCHEDULE = "schedule"
+    const val LATITUDE = "latitude"
+    const val LONGITUDE = "longitude"
+    const val NICKNAME = "nickname"
+    const val APP_PREFERENCES = "appPreferences"
+    const val ACCESS_TOKEN = "accessToken"
+    const val REFRESH_TOKEN = "refreshToken"
+    const val CONFIRMED = "confirmed"
+    const val CREATION_TIME = "creationTime"
+    const val SUBJECT = "subject"
+    const val EMAIL = "email"
+    const val RESOLVED = "resolved"
+    const val DATA = "data"
+    const val PAGE_NUMBER = "pageNumber"
+    const val PAGE_SIZE = "pageSize"
+    const val TOTAL_PAGES = "totalPages"
+    const val TOTAL_ENTITIES = "totalEntities"
+    const val ERROR_MESSAGE = "errorMessage"
+}
