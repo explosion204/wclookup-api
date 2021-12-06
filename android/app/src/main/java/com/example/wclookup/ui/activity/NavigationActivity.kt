c1a57b55e6cecf2aa2007f02dab1dfcf15bc2c65
ChoCoBo1712 <BigBen1712@mail.ru>
2021-12-06 23:01:03 +0300

    add burger nav menu

---
+++ android/app/src/main/java/com/example/wclookup/ui/activity/NavigationActivity.kt
@@ -0,0 +1,50 @@
+package com.example.wclookup.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.wclookup.R
+import com.example.wclookup.databinding.ActivityNavigationBinding
+import com.example.wclookup.ui.fragment.AuthFragment
+import com.google.android.material.navigation.NavigationView
+
+class NavigationActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityNavigationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.app_bar_navigation, AuthFragment())
+            .commit()
+
+        binding = ActivityNavigationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarNavigation.toolbar)
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_navigation)
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_map, R.id.nav_toilets, R.id.nav_auth
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_navigation)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
